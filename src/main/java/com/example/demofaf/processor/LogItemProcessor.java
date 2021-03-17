a864cbb788de2a2548a02a450dcc13ce8f8dc67c
rblasco <rblasco@lynxview.es>
2021-03-17 14:11:43 +0100

    Fixed bug de conversión de float

--- src/main/java/com/example/demofaf/processor/LogItemProcessor.java
+++ src/main/java/com/example/demofaf/processor/LogItemProcessor.java
@@ -6,6 +6,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.file.transform.FieldSet;
 
+import java.sql.Time;
+import java.util.Date;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,15 +20,34 @@ public class LogItemProcessor implements ItemProcessor<FieldSet, LogItem> {
 
     @Override
     public LogItem process(FieldSet item) throws Exception {
-        final String regex = ".+?(?=\\[)\\[(.+?(?=\\:))\\:(.+?(?=\\s?\\+)).+?(?=\\\")\\\"(.+?(?=\\s?\\/))\\s?(.+?(?=\\\")).+?(?=\\:)\\:\\s?(.*$)";
+        System.out.println("ITEM : " + item);
+        final String regex = ".+?(?=\\[)\\[(.+?(?=\\:))\\:(.+?(?=\\s?\\+)).+?(?=\\\")\\\"(.+?(?=\\s?\\/))\\s?(.+?(?=\\\")).+?(?=\\:)\\:\\s?(.+?(?=\\]))";
 
         final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
         final Matcher matcher = pattern.matcher(item.toString());
-
         while (matcher.find()) {
-            //System.out.println("Full match: " + matcher.group(0));
             for (int i = 1; i <= matcher.groupCount(); i++) {
                 System.out.println("Group " + i + ": " + matcher.group(i));
+                LogItem logItemNow = new LogItem();
+                logItemNow.setJO002SERVIDOR("server_nose");
+                String result = matcher.group(i);
+                switch (i){
+                    /*case 1: DateFormat formatterD = new SimpleDateFormat("dd/MM/yyyy");
+                        Date date = (Date) formatterD.parse(result);
+                        logItemNow.setJO002FECHA(date);
+                        break;*/
+                    /*case 2: DateFormat formatter = new SimpleDateFormat("HH:mm");
+                        Time t = (Time) formatter.parse(result);
+                        logItemNow.setJO002HORA(t);
+                        break;*/
+                    case 3: logItemNow.setJO002TIPO(result);
+                        break;
+                    case 4: logItemNow.setJO002OPERACION(result);
+                        break;
+                    case 5: double valueNumber = Double.parseDouble(result);
+                        logItemNow.setJO002TIEMPOMEDIO(valueNumber);
+                        break;
+                }
             }
         }
         return null;
